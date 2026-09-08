fb9ac06e11e7f9e236b70dd093cc79843a0b7abc
Denis Zaharchenko <denchick1546450@gmail.com>
2026-09-08 20:16:20 +0300

    add solution

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './styles/index.scss';
 import { App } from './App';
 import { CartProvider } from './modules/shared/context/CartContext';
 import { FavoritesProvider } from './modules/shared/context/FavoritesContext';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
-  <BrowserRouter>
+  <HashRouter>
     <CartProvider>
       <FavoritesProvider>
         <App />
       </FavoritesProvider>
     </CartProvider>
-  </BrowserRouter>,
+  </HashRouter>,
 );
